c7146cf67a9d458622b53c7991b2d8a1f5fe08a6
Josh Yang <spadeg39475@gmail.com>
2019-09-21 15:39:35 +0800

    build test

--- webpack/base.js
+++ webpack/base.js
@@ -9,7 +9,7 @@ module.exports = {
   entry:"./src/index.js",
   output:{ 
     path:path.resolve("dist"),
-    publicPath: './dist',
+    publicPath: '/',
     filename:"index_bundle.js" 
   },
   module: {
