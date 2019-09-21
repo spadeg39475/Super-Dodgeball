94e79a50e620d640d0922baaea88dccf386424fd
Josh Yang <spadeg39475@gmail.com>
2019-09-21 16:26:35 +0800

    build test

--- webpack/base.js
+++ webpack/base.js
@@ -47,7 +47,7 @@ module.exports = {
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
         // use: "file-loader"
-        use: 'url-loader?limit=8192&name=images/[hash:8].[name].[ext]'
+        use: 'url-loader?limit=8192&name=materials/img/[hash:8].[name].[ext]'
       }
     ]
   },
